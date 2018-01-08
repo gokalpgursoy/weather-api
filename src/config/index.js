a5fc8839b499333e9437907fd2a1fb187ec80be7
Gokalp Gursoy <gokalp@gokalpgursoy.com>
2018-01-08 15:01:22 +0300

    env cancelled for heroku

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,32 @@
+// module.exports = {
+//   port: process.env.PORT || 3000,
+//   db: {
+//     database: process.env.DB_NAME,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     options: {
+//       dialect: 'mysql',
+//       host: process.env.HOST,
+//       requestTimeout: 20000,
+//       force: false,
+//       logging: false,
+//     },
+//   },
+//   authentication: {
+//     jwtSecret: process.env.JWT_SECRET,
+//     expiresIn: process.env.EXPIRES_IN,
+//   },
+// };
+
 module.exports = {
   port: process.env.PORT || 3000,
   db: {
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    database: 'db9DF903',
+    user: 'user9DF903',
+    password: 'BYsv69W3',
     options: {
       dialect: 'mysql',
-      host: process.env.HOST,
+      host: '94.73.149.214',
       requestTimeout: 20000,
       force: false,
       logging: false,
